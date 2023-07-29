d6e1c76299871102b2b600a787f37b1cf1f05d74
ranji <email>
2023-07-29 11:55:30 +0530

    udemyjonas

---
+++ api/models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      max: 500,
+    },
+    game: { type: String, default: "nogame" },
+    rating: { type: Number, default: 0 },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
